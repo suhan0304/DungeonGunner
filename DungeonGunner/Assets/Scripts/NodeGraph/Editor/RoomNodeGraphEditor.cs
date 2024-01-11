b75af3d6cd1297d192fa5ebac4175d9a7f207895
ZerrY <suhan0304@gmail.com>
2024-01-12 05:16:52 +0900

    Update RoomNodeGraphEditor.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Codice.Client.Common.GameUI;
 
 public class RoomNodeGraphEditor : EditorWindow //ÆíÁý±â
 {
+    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
 
+    private static void OpenWindow()
+    {
+        GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
 }
