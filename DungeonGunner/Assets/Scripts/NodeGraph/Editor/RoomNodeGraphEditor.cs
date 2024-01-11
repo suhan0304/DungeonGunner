2ffaf8e0513066cf1f58a7eafdc1f28d9b4a2321
ZerrY <suhan0304@gmail.com>
2024-01-12 05:25:18 +0900

    Update RoomNodeGraphEditor.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,11 +5,23 @@ using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow //ÆíÁý±â
 {
+    private GUIStyle roomNodeStyle;
+
     [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
 
     private static void OpenWindow()
     {
         GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnEnable()
+    {
+        // Define node layout style
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node1") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
     }
 
     /// Draw Editor GUI
