305b9fdd06c11438d0844e054b40fa35155108fc
ZerrY <suhan0304@gmail.com>
2024-01-13 03:29:07 +0900

    Update HelperUtilities.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/Utilities/HelperUtilities.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Utilities/HelperUtilities.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HelperUtilities
 {
     /// <summary>
-    /// Empty String Debug check 
-    ///문자열(stringToCheck)이 공백이면 Log 출력하고 True 리턴해주는 검사기
+    /// Empty String Debug check - 문자열(stringToCheck)이 공백이면 Log 출력하고 True 리턴해주는 검사기
     /// <returns></returns>
     public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
     {
@@ -17,4 +17,24 @@ public class HelperUtilities
         }
         return false;
     }
+
+    public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectToCheck)
+    {
+        bool error = false;
+        int count = 0;
+
+        foreach(var item in enumerableObjectToCheck)
+        {
+            if (item == null)
+            {
+                Debug.Log(fieldName + " has null values in object " + thisObject.name.ToString());
+                error = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+
 }
