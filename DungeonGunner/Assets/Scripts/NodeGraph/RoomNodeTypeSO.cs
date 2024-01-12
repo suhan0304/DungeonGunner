8849c640090239c811a145b9bda04f224ecabe1d
ZerrY <suhan0304@gmail.com>
2024-01-13 03:09:40 +0900

    Create RoomNodeTypeSO.cs

---
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeType", menuName = "Scriptable Objects/Dungeon/Room Node Type")]
+public class RoomNodeTypeSO : ScriptableObject
+{
+    public string roomNodeTypeName;
+
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+
+
+
+}
