70c53e5c2aee72ba14adeddfea9fa08283ffa2bb
ZerrY <suhan0304@gmail.com>
2024-01-13 03:33:03 +0900

    Update RoomNodeTypeListSO.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -13,4 +13,13 @@ public class RoomNodeTypeListSO : ScriptableObject
     [Tooltip("This list should be populated with all the RoomNodeTypeSO for the game - it is used instead of an enum")]
     #endregion
     public List<RoomNodeTypeSO> list;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(list), list);
+    }
+#endif
+    #endregion
 }
