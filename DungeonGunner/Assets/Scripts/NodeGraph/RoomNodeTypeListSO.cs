a97deb94f588d8aa7bf2b0683fbd1975a569e9c8
ZerrY <suhan0304@gmail.com>
2024-01-13 03:24:01 +0900

    Create RoomNodeTypeListSO.cs

---
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeTypeListSO", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
+public class RoomNodeTypeListSO : ScriptableObject
+{
+    #region Header ROOM NODE TYPE LIST
+    [Space(10)]
+    [Header("ROOM NODE TYPE LIST")]
+    
+    public List<RoomNodeTypeSO> list;
+}
