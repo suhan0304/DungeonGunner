425e512cf4587390500f6470f7b9e839965187bf
ZerrY <suhan0304@gmail.com>
2024-01-13 03:24:42 +0900

    Update RoomNodeTypeListSO.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -8,6 +8,9 @@ public class RoomNodeTypeListSO : ScriptableObject
     #region Header ROOM NODE TYPE LIST
     [Space(10)]
     [Header("ROOM NODE TYPE LIST")]
-    
+    #endregion
+    #region Tooltip
+    [Tooltip("This list should be populated with all the RoomNodeTypeSO for the game - it is used instead of an enum")]
+    #endregion
     public List<RoomNodeTypeSO> list;
 }
