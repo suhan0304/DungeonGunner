e22369931b7570d79c3ded40a4fa94ddad3d1cd5
ZerrY <suhan0304@gmail.com>
2024-01-13 03:02:10 +0900

    Update RoomNodeGraphSO.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomNodeGraphSO : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Objects/Dungeon/Room Node Graph")]
+public class RoomNodeGraphSO : ScriptableObject
+{ 
+
 }
