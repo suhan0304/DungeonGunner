0f215b496fbc5644a8cb333d2647af41ceeada86
ZerrY <suhan0304@gmail.com>
2024-01-15 03:08:59 +0900

    Update RoomNodeGraphEditor.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
+using Unity.VisualScripting;
 
 public class RoomNodeGraphEditor : EditorWindow //편집기
 {
     private GUIStyle roomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeTypeListSO roomNodeTypeList;
 
     //Node layout Values
     private const float nodeWidth = 160f;
@@ -14,7 +16,6 @@ public class RoomNodeGraphEditor : EditorWindow //편집기
     private const int nodeBorder = 12;
 
     [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
-
     private static void OpenWindow()
     {
         GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
@@ -28,17 +29,32 @@ public class RoomNodeGraphEditor : EditorWindow //편집기
         roomNodeStyle.normal.textColor = Color.white;
         roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
         roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+        // Load Room Node types
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
+    }
+
+    /// <summary>
+    /// Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+    /// </summary>
+
+    [OnOpenAsset(0)] // Need the namespace UnityEditor.Callbacks
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
     }
 
     /// Draw Editor GUI
     private void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        EditorGUILayout.LabelField("Node 1");
-        GUILayout.EndArea();
-
-        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        EditorGUILayout.LabelField("Node 1");
-        GUILayout.EndArea();
     }
 }
