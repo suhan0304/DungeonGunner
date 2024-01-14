074b4d3e9ec7c9af994b32d5ce445bbef42c2e33
ZerrY <suhan0304@gmail.com>
2024-01-15 02:33:23 +0900

    Update GameResources.cs

--- DungeonGunner/Assets/Scripts/GameManager/GameResources.cs
+++ DungeonGunner/Assets/Scripts/GameManager/GameResources.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class GameResources : MonoBehaviour
 {
     private static GameResources instance;
+
+    public static GameResources Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = Resources.Load<GameResources>("GameResources");
+            }
+            return instance;
+        }
+    }
 }
