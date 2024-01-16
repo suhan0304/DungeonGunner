5e479e42d2a1f02619f92bfe5493cd301d9a5ed0
ZerrY <suhan0304@gmail.com>
2024-01-17 04:03:24 +0900

    Update RoomNodeSO.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 
 public class RoomNodeSO : ScriptableObject
 {
-    public string id;
-    public List<string> parentRoomNodeIDList = new List<string>();
-    public List<string> childRoomNodeIDList = new List<string>();
+    [HideInInspector] public string id;
+    [HideInInspector] public List<string> parentRoomNodeIDList = new List<string>();
+    [HideInInspector] public List<string> childRoomNodeIDList = new List<string>();
     [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
     public RoomNodeTypeSO roomNodeType;
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
