4b68b4a2211f76196565f3f5db8bc455ec99a512
ZerrY <suhan0304@gmail.com>
2024-01-17 03:38:19 +0900

    Update RoomNodeGraphSO.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -9,4 +9,21 @@ public class RoomNodeGraphSO : ScriptableObject
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
     [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
     [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
+
+    #region Editor Code
+
+    //The following code should only run in the Unity Editor
+#if UNITY_EDITOR
+
+    [HideInInspector] public RoomNodeSO roomNodeToDrawLineFrom = null;
+    [HideInInspector] public Vector2 linePosition;
+    
+    public void SetNodeToDrawConnectionLineFrom(RoomNodeSO node, Vector2 position)
+    {
+        roomNodeToDrawLineFrom = node;
+        linePosition = position;
+    }
+
+#endif
+    #endregion
 }
