cd7453907a760929bb0d5ba4bd841108c87a92a8
ZerrY <suhan0304@gmail.com>
2024-01-19 04:43:59 +0900

    Update Settings.cs

--- DungeonGunner/Assets/Scripts/Misc/Settings.cs
+++ DungeonGunner/Assets/Scripts/Misc/Settings.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Settings 
+public static class Settings
 {
-    
+    #region ROOM SETTINGS
+
+    public const int maxChildCorridors = 3; // Max number number of child corridors leading from a room - maximum should be 3 although this is not recommended
+                                            // since it can cause the dungeon building to fail since the rooms are more likely to not fit togther
+
+
+    #endregion
 }
