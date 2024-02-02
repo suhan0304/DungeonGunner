c9aec9137410ecde1d7d7dbc7769c6f1966f0a55
ZerrY <suhan0304@gmail.com>
2024-02-03 01:50:26 +0900

    Update Doorway.cs

--- DungeonGunner/Assets/Scripts/Dungeon/Doorway.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/Doorway.cs
@@ -5,6 +5,11 @@ public class Doorway
 {
     public Vector2Int position;
     public Orientation orientation;
+    public GameObject doorPrefab;
 
+    #region Header
+    [Header("The Uppder Left Position To Start Copying From")]
+    #endregion
+    public Vector2Int doorwayStartCopyPosition;
 
 }
