28b837b2453679dcd45042a3bbcb53c60b25fdd7
ZerrY <suhan0304@gmail.com>
2024-02-03 01:55:13 +0900

    Create RoomTemplateSO.cs

---
+++ DungeonGunner/Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Dungeon/Room")]
+public class RoomTemplateSO : ScriptableObject
+{
+    [HideInInspector] public string guid;
+
+
+}
