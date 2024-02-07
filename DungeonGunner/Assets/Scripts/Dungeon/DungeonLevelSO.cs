35b005a1cbf18f96b8570f91fe710edb713bfe49
ZerrY <suhan0304@gmail.com>
2024-02-08 04:59:29 +0900

    Update DungeonLevelSO.cs

--- DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -10,7 +10,6 @@ public class DungeonLevelSO : ScriptableObject
     [Space(10)]
     [Header("BASIC LEVEL DETAILS")]
     #endregion Header BASIC LEVEL DETAILS
-
     #region Tooltip
     [Tooltip("The name for the level")]
     #endregion Tooltip
@@ -21,11 +20,19 @@ public class DungeonLevelSO : ScriptableObject
     [Space(10)]
     [Header("ROOM TEMPLATES FOR LEVEL")]
     #endregion Header ROOM TEMPLATES FOR LEVEL
-
     #region Tooltip
     [Tooltip("Populate the list with the room templates that you want to be part of the level. You need to ensure that room templates are included for all room node types that are specified in the Room Node Graphs for the level.")]
     #endregion Tooltip
 
     public List<RoomTemplateSO> roomTemplateList;
 
+    #region Header ROOM NODE GRAPHS FOR LEVEL
+    [Space(10)]
+    [Header("ROOM NODE GRAPHS FOR LEVEL")]
+    #endregion Header ROOM NODE GRAPHS FOR LEVEL
+    #region Tooltip
+    [Tooltip("Populate this list with the room node graphs which should be randomly selected from for the level.")]
+    #endregion Tooltip
+
+    public List<RoomNodeGraphSO> roomNodeGraphList;
 }
