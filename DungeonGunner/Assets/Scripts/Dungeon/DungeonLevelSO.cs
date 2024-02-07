dc5709c6f7b8ce2eeffe0733a3b94bde928d3b89
ZerrY <suhan0304@gmail.com>
2024-02-08 04:56:13 +0900

    Update DungeonLevelSO.cs

--- DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DungeonLevel_", menuName = "Scriptable Objects/Dungeon/Dungeon Level")]
 public class DungeonLevelSO : ScriptableObject
 {
-    
+    #region Header BASIC LEVEL DETAILS
+    [Space(10)]
+    [Header("BASIC LEVEL DETAILS")]
+    #endregion Header BASIC LEVEL DETAILS
+
 }
