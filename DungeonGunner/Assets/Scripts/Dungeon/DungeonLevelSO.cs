8ee1cc4fd30df3bd876e6fc478aa8339a8d238c9
ZerrY <suhan0304@gmail.com>
2024-02-08 04:55:14 +0900

    Update DungeonLevelSO.cs

--- DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DungeonLevelSO : MonoBehaviour
+[CreateAssetMenu(fileName = "DungeonLevel_", menuName = "Scriptable Objects/Dungeon/Dungeon Level")]
+public class DungeonLevelSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
