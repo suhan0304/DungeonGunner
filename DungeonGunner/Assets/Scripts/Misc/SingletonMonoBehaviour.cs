640f28d60ef90786c32e736623dd5e49c0f3f5b9
ZerrY <suhan0304@gmail.com>
2024-02-12 01:05:11 +0900

    Update SingletonMonoBehaviour.cs

--- DungeonGunner/Assets/Scripts/Misc/SingletonMonoBehaviour.cs
+++ DungeonGunner/Assets/Scripts/Misc/SingletonMonoBehaviour.cs
@@ -1,16 +1,5 @@
 using UnityEngine;
 
-public class SingletonMonoBehaviour : MonoBehaviour
+public abstract class SingletonMonoBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
