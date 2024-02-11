bf309d87b889503ff61ebc8ec18e496fc1f4caab
ZerrY <suhan0304@gmail.com>
2024-02-12 01:05:53 +0900

    Update SingletonMonoBehaviour.cs

--- DungeonGunner/Assets/Scripts/Misc/SingletonMonoBehaviour.cs
+++ DungeonGunner/Assets/Scripts/Misc/SingletonMonoBehaviour.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 
 public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
 }
