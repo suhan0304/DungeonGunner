22b0052196cc28007de084ad0f71b870d0ba29ea
ZerrY <suhan0304@gmail.com>
2024-02-12 01:09:28 +0900

    Update TestOtherComponent.cs

--- DungeonGunner/Assets/Scripts/TestOtherComponent.cs
+++ DungeonGunner/Assets/Scripts/TestOtherComponent.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class TestOtherComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        TestGameManager.Instance.TestMethod();
     }
 }
