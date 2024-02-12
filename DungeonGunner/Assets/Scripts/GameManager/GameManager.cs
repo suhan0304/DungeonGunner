f463f599746c21abcd2f0724a5f9aca7cd057280
ZerrY <suhan0304@gmail.com>
2024-02-13 04:09:31 +0900

    Update GameManager.cs

--- DungeonGunner/Assets/Scripts/GameManager/GameManager.cs
+++ DungeonGunner/Assets/Scripts/GameManager/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     [HideInInspector] public GameState gameState;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
