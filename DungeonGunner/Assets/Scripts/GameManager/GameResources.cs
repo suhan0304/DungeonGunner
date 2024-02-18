8090bda8c31c9d77baf7fbf82554295ef3947ddf
ZerrY <suhan0304@gmail.com>
2024-02-19 03:58:31 +0900

    Update GameResources.cs

--- DungeonGunner/Assets/Scripts/GameManager/GameResources.cs
+++ DungeonGunner/Assets/Scripts/GameManager/GameResources.cs
@@ -28,4 +28,14 @@ public class GameResources : MonoBehaviour
 
     public RoomNodeTypeListSO roomNodeTypeList;
 
+    #region Header MATERIALS
+    [Space(10)]
+    [Header("MATERIALS")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Dimmed Material")]
+    #endregion
+
+    public Material dimmedMaterial;
+
 }
