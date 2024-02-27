937b5e2df1a13c29743d99b62a0a9c04357ee5af
ZerrY <suhan0304@gmail.com>
2024-02-28 04:37:59 +0900

    Update DungeonBuilder.cs

--- DungeonGunner/Assets/Scripts/Dungeon/DungeonBuilder.cs
+++ DungeonGunner/Assets/Scripts/Dungeon/DungeonBuilder.cs
@@ -223,7 +223,8 @@ public class DungeonBuilder : SingletonMonoBehaviour<DungeonBuilder>
         {
             switch (doorwayParent.orientation)
             {
-
+                case Orientation.north:
+                case Orientation.south:
             }
         }
     }
